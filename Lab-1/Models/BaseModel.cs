b065fd66ae41690dbf030e66c0e544f5db2ec65a
hramirez2240 <hramirez2298@gmail.com>
2022-03-27 11:14:12 -0400

    Third Commit

--- Lab-1/Models/BaseModel.cs
+++ Lab-1/Models/BaseModel.cs
@@ -10,18 +10,5 @@ namespace Lab_1.Models
         public string Name { get; set; }
         public int Age { get; set; }
         public string Gender { get; set; }
-
-        public virtual void Verification()
-        {
-            if (Age >= 18)
-            {
-                Console.WriteLine("Usted es mayor de edad");
-            }
-
-            else
-            {
-                Console.WriteLine("Usted es menor de edad");
-            }
-        }
     }
 }
